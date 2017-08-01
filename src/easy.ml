1b09dfe045174a34b9b0feabd64bab9271777b69
Valentin Reis <valentin.reis@gmail.com>
2017-08-01 08:00:17 +0200

    cosmetic

--- src/easy.ml
+++ src/easy.ml
@@ -21,22 +21,22 @@ module type Primary = sig
 end
 
 module MakeGreedyPrimary
-  (C:Metrics.Criteria)
-  (S:SchedulerParam)
+    (C:Metrics.Criteria)
+    (S:SchedulerParam)
   : Primary =
 struct
   let desc = C.desc
   let crit = C.criteria S.jobs
   let reorder ~system:s ~now:now ~log:log =
-        let lv =
-          let process_waiting i = match crit s now i with
-            |Value v -> (i,v,[])
-            |ValueLog (v,l) -> (i,v,l)
-          in List.map process_waiting s.waiting
-        in
-          (lv |> List.fold_left (fun acc (i,_,x) -> ((float_of_int now)::(float_of_int i)::x)::acc) log),
-          lv |> List.sort (compose_binop Pervasives.compare (fun (_,x,_) -> -. x))
-      |> List.map (fun (x,_,_) -> x)
+    let lv =
+      let process_waiting i = match crit s now i with
+        |Value v -> (i,v,[])
+        |ValueLog (v,l) -> (i,v,l)
+      in List.map process_waiting s.waiting
+    in
+    (lv |> List.fold_left (fun acc (i,_,x) -> ((float_of_int now)::(float_of_int i)::x)::acc) log),
+    lv |> List.sort (compose_binop Pervasives.compare (fun (_,x,_) -> -. x))
+    |> List.map (fun (x,_,_) -> x)
 end
 
 module type Threshold = sig
@@ -44,9 +44,9 @@ module type Threshold = sig
 end
 
 module MakeThreshold
-  (T:Threshold)
-  (P:Primary)
-  (S:SchedulerParam)
+    (T:Threshold)
+    (P:Primary)
+    (S:SchedulerParam)
   : Primary =
 struct
   let desc = P.desc
@@ -70,17 +70,17 @@ module type ProbaPolParam = sig
 end
 
 module MakeProbabilisticPrimary
-  (P:ProbaPolParam)
-  (S:SchedulerParam)
+    (P:ProbaPolParam)
+    (S:SchedulerParam)
   : Primary =
 struct
   let desc = "probabilistic."
   let pick_random_weighted l=
     let r = Random.float (float_of_int 1)
     in let rec nextrandom s i' = function
-      |(p,i)::is when s +. p >= r -> i
-      |(p,i)::is -> nextrandom (s +. p) i is
-      |[]-> i'
+        |(p,i)::is when s +. p >= r -> i
+        |(p,i)::is -> nextrandom (s +. p) i is
+        |[]-> i'
     in nextrandom 0. (snd (List.hd l)) l
 
   let reorder ~system:s ~now:now ~log:log =
@@ -96,11 +96,11 @@ struct
           in List.map (fun x -> x /. (max 0.000001 denom)) scaled_alphas
     in let crit = pick_random_weighted (BatList.map2 (fun p i -> (p,i)) probas P.criterias)
     in let process_waiting i = match crit S.jobs s now i with
-      |Value v -> (i,v)
-      |ValueLog (v,l) -> (i,v)
+        |Value v -> (i,v)
+        |ValueLog (v,l) -> (i,v)
     in let v = List.map process_waiting s.waiting
-      |> List.sort (compose_binop Pervasives.compare snd) 
-      |> List.map fst
+               |> List.sort (compose_binop Pervasives.compare snd) 
+               |> List.map fst
     in ([],v)
 end
 
@@ -110,8 +110,8 @@ module type HysteresisParam = sig
 end
 
 module MakeHysteresisPrimary
-  (P:HysteresisParam)
-  (S:SchedulerParam)
+    (P:HysteresisParam)
+    (S:SchedulerParam)
   : Primary =
 struct
   type features = float list [@@deriving protobuf {protoc}]
@@ -134,31 +134,31 @@ struct
 
   let reorder ~system:s ~now:now ~log:log = 
     let values = List.map 
-     (fun (_,f) -> Metrics.get_value (f S.jobs s now 0)) 
-     Metrics.features_system
+        (fun (_,f) -> Metrics.get_value (f S.jobs s now 0)) 
+        Metrics.features_system
     in let var = List.nth values 12
     in let t1,t2 = P.thresholds
     in let high = max t1 t2
     in let low = min t1 t2
     in let pol1, pol2 = 
-      if t1 < t2 then
-        M1.reorder, M2.reorder
-      else
-        M2.reorder, M1.reorder
+         if t1 < t2 then
+           M1.reorder, M2.reorder
+         else
+           M2.reorder, M1.reorder
     in if (not !memory) then  (*if we are on P_low*)
       if var > high then
         (memory := true;
-        pol2 ~system:s ~now:now ~log:log)
+         pol2 ~system:s ~now:now ~log:log)
       else
         (assert(!memory = false);
-        pol1 ~system:s ~now:now ~log:log)
+         pol1 ~system:s ~now:now ~log:log)
     else
-      if var < low then (*if we are on P_high*)
-        (memory := false;
-        pol1 ~system:s ~now:now ~log:log)
-      else
-        (assert(!memory = true);
-        pol2 ~system:s ~now:now ~log:log)
+    if var < low then (*if we are on P_high*)
+      (memory := false;
+       pol1 ~system:s ~now:now ~log:log)
+    else
+      (assert(!memory = true);
+       pol2 ~system:s ~now:now ~log:log)
 end
 
 module type ContextualParam = sig
@@ -168,8 +168,8 @@ module type ContextualParam = sig
 end
 
 module MakeContextualPrimary
-  (P:ContextualParam)
-  (S:SchedulerParam)
+    (P:ContextualParam)
+    (S:SchedulerParam)
   : Primary =
 struct
   type features = float list [@@deriving protobuf {protoc}]
@@ -183,11 +183,11 @@ struct
     let criteria =
       let nn_pick (s:system) (now:int)  = 
         let values = List.map 
-         (fun (_,f) -> Metrics.get_value (f S.jobs s now 0)) 
-         Metrics.features_system
+            (fun (_,f) -> Metrics.get_value (f S.jobs s now 0)) 
+            Metrics.features_system
         in let packet = 
-          let e = Protobuf.Encoder.create () 
-          in (features_to_protobuf values e; Protobuf.Encoder.to_string e)
+             let e = Protobuf.Encoder.create () 
+             in (features_to_protobuf values e; Protobuf.Encoder.to_string e)
         in begin
           ZMQ.Socket.send requester packet;
           let reply = ZMQ.Socket.recv requester 
@@ -195,15 +195,48 @@ struct
         end
       in List.nth P.policies (nn_pick s now)
     in let lv =
-          let process_waiting i = match criteria S.jobs s now i with
-            |Value v -> (i,v,[])
-            |ValueLog (v,l) -> (i,v,l)
-          in List.map process_waiting s.waiting
-        in
-          lv |> List.fold_left (fun acc (i,_,x) ->
-                                  ((float_of_int now)::(float_of_int i)::x)::acc) log,
-          lv |> List.sort (compose_binop Pervasives.compare (fun (_,x,_) -> x))
-      |> List.map (fun (x,_,_) -> x)
+         let process_waiting i = match criteria S.jobs s now i with
+           |Value v -> (i,v,[])
+           |ValueLog (v,l) -> (i,v,l)
+         in List.map process_waiting s.waiting
+    in
+    lv |> List.fold_left (fun acc (i,_,x) ->
+        ((float_of_int now)::(float_of_int i)::x)::acc) log,
+    lv |> List.sort (compose_binop Pervasives.compare (fun (_,x,_) -> x))
+    |> List.map (fun (x,_,_) -> x)
+
+end
+
+module type OocvxParam = sig
+  val period : int
+  val policies : Metrics.criteria list
+end
+
+module MakeOocvxPrimary
+    (P:OocvxParam)
+    (S:SchedulerParam)
+  : Primary =
+struct
+  type features = float list
+  type pick = int
+  let desc = "resimulation"
+
+  let reorder ~system:s ~now:now ~log:log = 
+    let criteria =
+      let x = List.map 
+          (fun (_,f) -> Metrics.get_value (f S.jobs s now 0)) 
+          Metrics.features_system
+      in List.nth P.policies 1
+    in let lv =
+         let process_waiting i = match criteria S.jobs s now i with
+           |Value v -> (i,v,[])
+           |ValueLog (v,l) -> (i,v,l)
+         in List.map process_waiting s.waiting
+    in
+    lv |> List.fold_left (fun acc (i,_,x) ->
+        ((float_of_int now)::(float_of_int i)::x)::acc) log,
+    lv |> List.sort (compose_binop Pervasives.compare (fun (_,x,_) -> x))
+    |> List.map (fun (x,_,_) -> x)
 
 end
 
@@ -223,8 +256,8 @@ end
 (*list jobs eligible for backfilling*)
 
 module MakeGreedySecondary
-  (C:Metrics.Criteria)
-  (S:SchedulerParam)
+    (C:Metrics.Criteria)
+    (S:SchedulerParam)
   : Secondary =
 struct
 
@@ -237,13 +270,13 @@ struct
   let crit = C.criteria S.jobs
 
   let pick
-        ~system:s
-        ~now:now
-        ~backfillable:bfable
-        ~reservationWait:restime
-        ~reservationID:resjob
-        ~reservationFree:free'
-        ~freeNow:free =
+      ~system:s
+      ~now:now
+      ~backfillable:bfable
+      ~reservationWait:restime
+      ~reservationID:resjob
+      ~reservationFree:free'
+      ~freeNow:free =
     let rec picknext f f' picked = function
       |[] -> picked
       |i::is ->
@@ -258,8 +291,8 @@ struct
           else
             picknext f f' picked is
     and sorted = List.sort 
-    (compose_binop Pervasives.compare (fun x -> -. Metrics.get_value (crit s now x))) 
-    bfable
+        (compose_binop Pervasives.compare (fun x -> -. Metrics.get_value (crit s now x))) 
+        bfable
     in picknext free free' [] sorted
 end
 
@@ -269,9 +302,9 @@ module type Scheduler = sig
 end
 
 module MakeEasyScheduler
-  (Primary:Primary)
-  (Secondary:Secondary)
-  (P:SchedulerParam)
+    (Primary:Primary)
+    (Secondary:Secondary)
+    (P:SchedulerParam)
   : Scheduler =
 struct
   let fj = Hashtbl.find P.jobs
@@ -288,43 +321,43 @@ struct
     let rec easy decision free = function
       | [] -> Simple decision
       | i :: is  -> let remaining = free-(fj i).q
-        in if remaining >= 0 then easy (decision@[i]) remaining is
-        else Backfill (decision,free,i,is)
+          in if remaining >= 0 then easy (decision@[i]) remaining is
+          else Backfill (decision,free,i,is)
     in easy [] free wq
 
   let reserve ~now:now ~free:free ~q:needed ~running:running ~decision:decision =
     let rec fits f = function
       | [] -> failwith "impossible to backfill this job. check MaxProcs."
-          | (t,i)::tis ->
-              let f' = f+ (fj i).q
-              in if f' >= needed then
-                let resaWait = t-now;
-                in ( assert ( resaWait > 0 ); ( resaWait, f'-needed ))
-                else
-                  fits (f') tis
-                    and projected =
-                      let sort = (List.sort (fun (t,_) (t',_) -> compare t t'))
-                      and project = (List.map (fun (t,i) -> (t+ (fj i).p_est,i)))
-                      in (running @ (List.map (fun i -> (now,i)) decision))
-          |> project |> sort
+      | (t,i)::tis ->
+          let f' = f+ (fj i).q
+          in if f' >= needed then
+            let resaWait = t-now;
+            in ( assert ( resaWait > 0 ); ( resaWait, f'-needed ))
+          else
+            fits (f') tis
+    and projected =
+      let sort = (List.sort (fun (t,_) (t',_) -> compare t t'))
+      and project = (List.map (fun (t,i) -> (t+ (fj i).p_est,i)))
+      in (running @ (List.map (fun i -> (now,i)) decision))
+         |> project |> sort
     in fits free projected
 
 
   let schedule now s log =
     let log, reordered = Primary.reorder ~system:s ~now:now ~log:log
     in match get_easy s.free reordered with
-      | Simple decision                     -> (decision,log)
-      | Backfill (decision, _, _, [])       -> (decision,log)
-      | Backfill (decision, free, id, rest) ->
-          assert (free >= 0);
-          let resaWait, resaFree =
-            reserve ~now:now ~free:free ~q:(fj id).q ~running:s.running
-              ~decision:decision
-          in let () = assert (resaWait>0)
-          in let () = assert (resaFree>=0)
-          in let backfilled =
-            Secondary.pick ~system:s ~now:now ~backfillable:rest
-              ~reservationWait:resaWait ~reservationID:id ~reservationFree:resaFree
-              ~freeNow:free
-          in ((decision @ backfilled), log)
+    | Simple decision                     -> (decision,log)
+    | Backfill (decision, _, _, [])       -> (decision,log)
+    | Backfill (decision, free, id, rest) ->
+        assert (free >= 0);
+        let resaWait, resaFree =
+          reserve ~now:now ~free:free ~q:(fj id).q ~running:s.running
+            ~decision:decision
+        in let () = assert (resaWait>0)
+        in let () = assert (resaFree>=0)
+        in let backfilled =
+             Secondary.pick ~system:s ~now:now ~backfillable:rest
+               ~reservationWait:resaWait ~reservationID:id ~reservationFree:resaFree
+               ~freeNow:free
+        in ((decision @ backfilled), log)
 end
